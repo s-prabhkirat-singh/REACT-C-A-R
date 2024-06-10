507fd90fc839bcf8fb5eb35b189fc189eb895bb6
Prabhkirat Singh <Prabhkiratsingh2278@hmail.com>
2024-06-10 10:11:28 +0530

    completed bgChanger Project

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
 
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [color,setColor]=useState("olive")
 
   return (
    <>
-   <div>
+   <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
+    <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+      <div className='flex flex-wrap justify-items-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl'>
+        <button onClick={()=>setColor("red")}
+        className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "red"}}>Red
+        </button>
+        <button onClick={()=>setColor("green")}
+        className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "green"}}>green
+        </button>
+        <button onClick={()=>setColor("blue")}
+        className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "blue"}}>blue
+        </button>
+       
+      </div>
+    </div>
 
    </div>
    </>
